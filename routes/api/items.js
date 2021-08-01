4d7eecedbf1f2e821ff7ad0c0897e628e5afddf2
Cicelia <ciceliasiu@gmail.com>
2021-08-01 03:55:38 -0700

    added react-app and added npm scripts to package.json

--- routes/api/items.js
+++ routes/api/items.js
@@ -30,10 +30,10 @@ router.post('/', (req, res) => {
 //@desc     Delete an Item
 //@access   Public
 router.delete('/:id', (req, res) => {
-    Item.findById(req.params.id).then(item => 
-        item.remove().then(() => res.json({success: true})))
-        .catch(err => res.status(404).json({success: false}));
-    });
+    Item.findById(req.params.id).then(item =>
+        item.remove().then(() => res.json({ success: true })))
+        .catch(err => res.status(404).json({ success: false }));
+});
 
 
 module.exports = router;
